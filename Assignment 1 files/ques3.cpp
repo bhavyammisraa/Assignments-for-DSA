e4b05cb88f83c7b9cc9bec5377a50b8fb4c72a70
bhavyammisraa <bmisra_be24@thapar.edu>
2025-08-12 23:30:10 +0530

    Add files via upload
    Solutions to the 5 Questions in the Assignment 1 of Data Structures.

---
+++ Assignment 1 files/ques3.cpp
@@ -0,0 +1,11 @@
+//Predict the Output of the following program
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    int arr[5] = {1};
+    for(i=0; i<5; i++) cout<<arr[i]<<" ";
+    return 0;
+}
+//The output comes out to be 1 0 0 0 0
